8e3b91506f2d4f618951d4bc4cee10e983f20b3e
Liam Cain <hi@liamca.in>
2022-03-20 16:30:00 -0400

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Menu, MenuItem, Plugin, TAbstractFile, TFolder } from "obsidian";
+
+export default class SearchFromDirectoryPlugin extends Plugin {
+  async onload() {
+    this.registerEvent(
+      this.app.workspace.on("file-menu", (menu: Menu, fileOrFolder: TAbstractFile) => {
+        if (fileOrFolder instanceof TFolder) {
+          menu.addItem((item: MenuItem) => {
+            item
+              .setTitle("Search in folder")
+              .setIcon("search")
+              .onClick(() => {
+                const folderPath = fileOrFolder.path;
+                (this.app as any).internalPlugins
+                  .getPluginById("global-search")
+                  .instance.openGlobalSearch(`path:"${folderPath}" tag:task/todo`);
+              });
+          });
+        }
+      })
+    );
+  }
+}
