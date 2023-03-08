62b637ae81dbde82310564dc40f5f6acd150b6fc
1C0D <63018905+1C0D@users.noreply.github.com>
2023-03-08 20:17:06 +0100

    cursor position at the end to enter search term

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,30 @@ import { Menu, MenuItem, Plugin, TAbstractFile, TFolder } from "obsidian";
 export default class SearchFromDirectoryPlugin extends Plugin {
   async onload() {
     this.registerEvent(
-      this.app.workspace.on(
-        "file-menu",
-        (menu: Menu, fileOrFolder: TAbstractFile) => {
-          if (fileOrFolder instanceof TFolder) {
-            menu.addItem((item: MenuItem) => {
-              item
-                .setTitle("Search in folder")
-                .setIcon("search")
-                .onClick(async () => {
-                  const folderPath = fileOrFolder.path;
-                  (this.app as any).internalPlugins
-                    .getPluginById("global-search")
-                    .instance.openGlobalSearch(`path:"${folderPath}" `);
-                  const searchInput = document.querySelector(".search-input-container input") as HTMLInputElement;
-                  setTimeout(() => {
-                    searchInput.setSelectionRange(searchInput.value.length, searchInput.value.length);
-                  }, 100);
-                });
-            });
-          }
+      this.app.workspace.on("file-menu", (menu: Menu, fileOrFolder: TAbstractFile) => {
+        if (fileOrFolder instanceof TFolder) {
+          menu.addItem((item: MenuItem) => {
+            item
+              .setTitle("Search in folder")
+              .setIcon("search")
+              .onClick(() => {
+                const folderPath = fileOrFolder.path;
+                (this.app as any).internalPlugins
+                  .getPluginById("global-search")
+                  .instance.openGlobalSearch(`path:"${folderPath}" `);
+                const searchInput = document.querySelector(
+                    ".search-input-container input"
+                  ) as HTMLInputElement;
+                setTimeout(() => {
+                    searchInput.setSelectionRange(
+                      searchInput.value.length,
+                      searchInput.value.length
+                    );
+                }, 100);
+              });
+          });
         }
-      )
+      })
     );
   }
 }
